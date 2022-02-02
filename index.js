acd320bdc333160bf423fed4ba1a406e3d9dae6c
Julian Waller <git@julusian.co.uk>
2022-02-02 16:43:46 +0000

    bulk replace system.emit calls

--- index.js
+++ index.js
@@ -116,7 +116,7 @@ instance.prototype.destroy = function() {
 instance.prototype.actions = function(system) {
 	var self = this;
 
-	self.system.emit('instance_actions', self.id, {
+	self.setActions({
 		'rpreset':     {
 			label: 'Recall Preset (Number)',
 			options: [
